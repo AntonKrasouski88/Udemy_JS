915ad5452c6688fb119bf3c062aca40b9992390a
AntonKrasouski88 <antohkad21@gmail.com>
2022-07-28 15:31:03 +0300

    Task: Sum of positive

--- js/codewars.js
+++ js/codewars.js
@@ -362,3 +362,20 @@ findMultiples(5, 27);
 
 //Task Quarter of the year
 const quarterOf = m => Math.ceil(m/3);
+
+//Sum of positive
+
+/* You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0. */
+
+function positiveSum(arr) {
+let num = 0;
+arr.map(i=> i > 0 ? num +=i: 0); 
+return console.log(num);
+}
+
+positiveSum([1,2,3,4,5]);
+positiveSum([1,-2,3,4,5]);
