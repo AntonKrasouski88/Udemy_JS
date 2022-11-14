8be5f26fff7eafb609696e87f85bd57471eb60e3
AntonKrasouski88 <antohkad21@gmail.com>
2022-11-14 12:18:14 +0300

    Script: Tasks with recurtion

--- js/script.js
+++ js/script.js
@@ -205,3 +205,112 @@ personalMovieDB.detectPersonalLevel();
 personalMovieDB.toggleVisibleMyDB(personalMovieDB.privat);
 personalMovieDB.writeYourGengers();
 personalMovieDB.showMyDB(personalMovieDB.privat);
+
+
+function pow (a, n) {
+    let result = 1;
+    for (let i = 0; i < n; i++) {
+        result *= a
+    }
+    console.log(result)
+}
+// Рекурсия(recursion)
+function pow(a,n) {
+    if(n === 1) {
+        return a
+    } else {
+        return a * pow(a, n-1)
+    }
+}
+
+pow(2,1) // 2
+pow(2,2) // 4
+pow(2,3) // 8
+pow(2,4) // 16
+
+let studens = {
+    js: [{
+        name: 'John',
+        progress: 100,
+    }, {
+        name: 'Ivan',
+        progress: 60,
+    }],
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress: 20,
+        }, {
+            name: 'Anna',
+            progress: 18,
+        }],
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }]
+    } 
+};
+
+function getTotalProgressByIteration (data) {
+    let total = 0;
+    let studens = 0;
+
+    for (let course of Object.values(data)) {
+        if(Array.isArray(course)) {
+            studens += course.length;
+            for (let i = 0; i < course.length; i++) {
+                total += course[i].progress;
+            } 
+    } else {
+        for (let subCourse of Object.values(course)) {
+            studens += subCourse.length;
+            for (let i = 0; i < subCourse.length; i++) {
+                total += subCourse[i].progress;
+            }
+        }
+    }
+}
+    
+    return total / studens
+}
+
+//console.log (getTotalProgressByIteration(studens))
+
+function getTotalProgressByRecursion (data) {
+    if(Array.isArray(data)) {
+        let total = 0;
+        for (let i = 0; i < data.length; i++) {
+            total += data[i].progress; 
+        }
+        return [total, data.length]
+    } else {
+        let total = [0, 0];
+        for (let subData of Object.values(data)) {
+            let subDataArr = getTotalProgressByRecursion(subData);
+            total[0] += subDataArr[0];
+            console.log(total[0])
+            total[1] += subDataArr[1];
+            console.log(total[1])
+
+        }
+        return total;
+    }
+}
+
+const result = getTotalProgressByRecursion(studens);
+console.log(result[0]/result[1]);
+
+
+function factorial (n) {
+    if (!Number.isInteger(n) || typeof n !== 'number') {
+        return ('Error')
+    }
+    if (n <= 1) {
+        return n = 1 
+    } else {
+        return n * factorial(n-1)
+    }
+}
+console.log(factorial ('dsfdsfs'))
+console.log(factorial (3)) 
+console.log(factorial (4.5))
