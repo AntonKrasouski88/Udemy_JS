e5228efb2ad5f88d5a2510aedc305a96d77dac84
AntonKrasouski88 <antohkad21@gmail.com>
2022-12-06 12:28:12 +0300

    Task: Reversed String

--- js/codewars.js
+++ js/codewars.js
@@ -464,3 +464,12 @@ function filter_list(l) {
 filter_list([1,2,'a','b'])
 filter_list([1,'a','b',0,15])
 filter_list([1,2,'aasf','1','123',123])
+
+
+//Task: Reversed Strings
+function solution(str){
+    const reverseStr = str.split('').reverse().join('');
+    console.log(reverseStr)
+}
+
+solution('world')
