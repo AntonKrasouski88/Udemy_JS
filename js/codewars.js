ec85b0e200125151e095754d1937950b3e3ce5be
AntonKrasouski88 <antohkad21@gmail.com>
2023-02-08 11:29:40 +0300

    Task: even, odd

--- js/codewars.js
+++ js/codewars.js
@@ -933,3 +933,22 @@ function solution(number){
 console.log(solution(10));
 console.log(solution(23));
 console.log(solution(-1));
+
+function findOutlier(integers){
+  let arr1 = integers.filter(el=> el%2 === 0);
+  let arr2 = integers.filter(el=>el%2 !== 0);
+  if (arr1.length  === 1 ) {
+    return arr1[0];
+  } else {
+    return arr2[0];
+  }
+  /* var even = integers.filter(a=>a%2==0);
+  var odd = integers.filter(a=>a%2!==0);
+  return even.length==1? even[0] : odd[0]; */
+}
+
+console.log(findOutlier([0, 1, 2]));
+console.log(findOutlier([1, 2, 3]));
+console.log(findOutlier([2,6,8,10,3]));
+console.log(findOutlier([0,0,3,0,0]));
+console.log(findOutlier([1,1,0,1,1]));
