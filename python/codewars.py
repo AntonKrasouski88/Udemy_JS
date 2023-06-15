0cf92a99922b754293c1b2af3208865f23a079a3
Anton Krasousky <antohkad21@gmail.com>
2023-06-15 22:05:20 +0300

    Task: Super Duper Easy

--- python/codewars.py
+++ python/codewars.py
@@ -1,8 +1,22 @@
 # Task: Even or Odd
 def even_or_odd(number):
-  if number % 2 == 0:
-    return "Even"
-  else:
-    return "Odd"
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
 
 print(even_or_odd(5))
+
+
+
+# Task: Super Duper Easy
+def problem(a):
+    if(type(a) == str):
+        return 'Error'
+    else:
+        return a * 50 + 6
+    
+
+print(problem(1))
+print(problem('Hello'))
