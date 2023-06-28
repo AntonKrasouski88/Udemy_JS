0749fc951d52f2bc09b77ba3e56e41eb48c8ad14
Anton Krasousky <antohkad21@gmail.com>
2023-06-28 21:05:10 +0300

    Task: Drink about

--- python/codewars.py
+++ python/codewars.py
@@ -9,7 +9,6 @@ def even_or_odd(number):
 print(even_or_odd(5))
 
 
-
 # Task: Super Duper Easy
 def problem(a):
     if(type(a) == str):
@@ -20,3 +19,24 @@ def problem(a):
 
 print(problem(1))
 print(problem('Hello'))
+
+n = 1600
+if (n%4 == 0 and n%100 != 0) or n%400 == 0:
+    print('YES')
+else:
+    print('NO')
+
+
+#Task: Drink about
+def people_with_age_drink(age):
+    if age < 14:
+        return 'drink toddy'
+    elif 13 < age < 18:
+        return 'drink coke'
+    elif 17 < age < 21:
+        return 'drink beer'
+    else:
+        return 'drink whisky'
+    
+
+print(people_with_age_drink(13))
