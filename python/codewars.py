1231283a42c07e323ea1cc606361167e3f8ddc63
Anton <antohkad21@gmail.com>
2023-07-02 15:49:19 +0300

    Task: amount of numbers 2

--- python/codewars.py
+++ python/codewars.py
@@ -113,3 +113,15 @@ def asymptoticApproximation (n):
 
 
 print(asymptoticApproximation(10))
+
+
+#Task: amount of numbers 2
+def amountOfNumbers2 (n):
+    total = 0
+    for i in range (1, n + 1):
+        if i**2%10 == 2 or i**2%10 == 5 or i**2%10 == 8:
+            total += i
+    return total
+
+
+print(amountOfNumbers2(10))
