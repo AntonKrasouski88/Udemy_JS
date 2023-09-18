460461815f90471723a368d92f12085ad27b4791
Anton Krasousky <antohkad21@gmail.com>
2023-09-18 23:21:42 +0300

    feat: task - find the first non-consecutive number

--- js/codewars.js
+++ js/codewars.js
@@ -1266,3 +1266,16 @@ function checkAlive (health) {
 }
 console.log(checkAlive(5));
 console.log(checkAlive(0));
+
+//Task: Find the first non-consecutive number
+function firstNonConsecutive (arr) {
+  for(let i = 0;i < arr.length - 1; i++) {
+    if(arr[i + 1] - arr[i] !== 1) {
+      return arr[i + 1]
+    }
+  }
+  return null
+}
+
+console.log(firstNonConsecutive([1,2,3,4,6,7,8]));
+console.log(firstNonConsecutive([1,2,3,4]));
