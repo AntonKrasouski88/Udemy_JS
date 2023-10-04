06261d2634e3607bd3ddfee6f863e4b63c0cb8cf
Anton Krasovski <antohkad21@gmail.com>
2023-10-04 22:05:21 +0300

    feat: task - sum of odd cubed numbers

--- js/codewars.js
+++ js/codewars.js
@@ -1581,3 +1581,27 @@ function alphabetPosition(text) {
 }
 
 console.log(alphabetPosition("@9|:o^4n"));
+
+//Task: Sum of Odd Cubed Numbwers
+function cubeOdd(arr) {
+  let count = 0;
+  for(let i = 0; i < arr.length; i++) {
+    if(!Number.isInteger(arr[i]) || Number.isNaN(arr[i])) {
+      return undefined
+    }
+    if(arr[i] % 2 !== 0) {
+      count += arr[i]**3
+    }
+  }
+  return count
+}
+
+/* let cubeOdd = a => {
+  var isNumeric = a.every(x=>!isNaN(x))
+  return isNumeric ? a.filter(n=>n%2).reduce((s,n)=>s+(n*n*n),0) : undefined
+} */
+
+console.log(cubeOdd([1, 2, 3, 4]));
+console.log(cubeOdd([-3,-2,2,3]));
+console.log(cubeOdd(["a",12,9,"z",42]));
+console.log(cubeOdd([5,12,9,NaN,9]));
